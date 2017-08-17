26e36c9a7d5b1a286c8f90fbd0b5c964a5cd187a
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-08-17 13:49:09 +1200

    Prior to input data
    After registering the context with dependency injection

--- ContosoUniversity/src/ContosoUniversity/Startup.cs
+++ ContosoUniversity/src/ContosoUniversity/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 using ContosoUniversity.Services;
