c9e6a0110be0f7297c4d8817d4d4273530a45e8b
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-09-09 20:46:52 +1200

    can upload file but not good

---
+++ QualityHat/src/QualityHat/Controllers/UploadFileController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace QualityHat.Controllers
+{
+    public class UploadFileController : Controller
+    {
+		//public IActionResult Index()
+		//{
+		//    return View();
+		//}
+
+		#region snippet1
+		[HttpPost("UploadFiles")]
+		public async Task<IActionResult> Post(IFormFile file)
+		{
+
+
+			// full path to file in temp location
+			//var filePath = Path.GetTempFileName();
+			var filePath = "./wwwroot/images/temp1.png";
+
+				if (file.Length > 0)
+				{
+					using (var stream = new FileStream(filePath, FileMode.Create))
+					{
+						await file.CopyToAsync(stream);
+					}
+				}
+
+			// process uploaded files
+			// Don't rely on or trust the FileName property without validation.
+
+			return Ok(new { filePath });
+		}
+		#endregion
+	}
+}
