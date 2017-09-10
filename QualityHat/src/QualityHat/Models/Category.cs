b5f69005d50fbac63a66cd903ef6348ee22a0571
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-09-10 12:19:31 +1200

    add name in authentication

--- QualityHat/src/QualityHat/Models/Category.cs
+++ QualityHat/src/QualityHat/Models/Category.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace QualityHat.Models
 {
-    public class Category
+	public class Category
     {
 		public int CategoryID { get; set; }	
 		public string Name { get; set; }
