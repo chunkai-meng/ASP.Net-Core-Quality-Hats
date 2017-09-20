e5ef632a10ee1118c5bbff488fe4ba3ed4d2ba1b
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-09-20 12:34:32 +1200

    add ViewModel

--- QualityHat/src/QualityHat/Models/ShoppingBagViewModels/ShoppingBagViewModel.cs
+++ QualityHat/src/QualityHat/Models/ShoppingBagViewModels/ShoppingBagViewModel.cs
@@ -2,8 +2,10 @@
 
 namespace QualityHat.Models.ShoppingBagViewModels
 {
-	public class ShoppingBagData
+	public class ShoppingBagViewModel
 	{
+		public Hat Hat { get; set; }
+		public Order Order { get; set; }
 		public int HatID { get; set; }
 		public string Name { get; set; }
 		public bool Selected { get; set; }
