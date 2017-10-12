250e98ffa199295e3b10a96d5b54bcf0c694b19f
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-10-12 17:27:10 +1300

    Stick footer to the bottom

--- QualityHat/src/QualityHat/wwwroot/js/site.js
+++ QualityHat/src/QualityHat/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your Javascript code.
+$(document).ready(function () {
+    var firstParagraph = $("p:first");
+    $("#addContent").on("click", function () {
+        $(".body-content").append(firstParagraph.clone());
+    });
+});
